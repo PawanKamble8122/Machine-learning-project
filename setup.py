1bee722165affa71c85cf35ac23331da7d66a8a4
PawanKamble8122 <pawankamble8122@gmail.com>
2025-12-10 14:24:46 +1100

    folder add

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Machine-learning-project",  # Replace with your package name
+    version="0.0.1",
+    author="Pawan Kamble",  # Replace with your name
+    author_email="pawankamble8122@gmail.com",
+    description="A machine learning project setup"
+)
